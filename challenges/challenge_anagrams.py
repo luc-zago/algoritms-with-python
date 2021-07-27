7f26fe0765420e68d46e7e6a142616636f635be6
Lucas <luc.zago@hotmail.com>
2021-07-27 14:59:33 -0300

    Nova alteração no algoritmo de ordenação

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,17 +1,18 @@
-def selection_sort(array):
-    for i in range(len(array)):
-        min_idx = i
-        for j in range(i + 1, len(array)):
-            if array[min_idx] > array[j]:
-                min_idx = j
-        array[i], array[min_idx] = array[min_idx], array[i]
+def quick_sort(inlist):
+    if inlist == []:
+        return []
+    else:
+        pivot = inlist[0]
+        lesser = quick_sort([x for x in inlist[1:] if x < pivot])
+        greater = quick_sort([x for x in inlist[1:] if x >= pivot])
+        return lesser + [pivot] + greater
 
 
 def is_anagram(first_string, second_string):
     """Faça o código aqui."""
-    first_string, second_string = list(first_string), list(second_string)
-    selection_sort(first_string)
-    selection_sort(second_string)
+    first_string, second_string = quick_sort(list(first_string)), quick_sort(
+        list(second_string)
+    )
     if first_string == second_string:
         return True
     return False

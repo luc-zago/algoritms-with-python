5f38c5adcce4d3b99db687ceae2deac07ed79e6b
Lucas <luc.zago@hotmail.com>
2021-07-27 14:53:08 -0300

    Alterado algoritmo de ordenação

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,18 +1,17 @@
-def insertion_sort(array):
-    for i in range(1, len(array)):
-        key = array[i]
-        j = i - 1
-        while j >= 0 and key < array[j]:
-            array[j + 1] = array[j]
-            j -= 1
-        array[j + 1] = key
+def selection_sort(array):
+    for i in range(len(array)):
+        min_idx = i
+        for j in range(i + 1, len(array)):
+            if array[min_idx] > array[j]:
+                min_idx = j
+        array[i], array[min_idx] = array[min_idx], array[i]
 
 
 def is_anagram(first_string, second_string):
     """Faça o código aqui."""
     first_string, second_string = list(first_string), list(second_string)
-    insertion_sort(first_string)
-    insertion_sort(second_string)
+    selection_sort(first_string)
+    selection_sort(second_string)
     if first_string == second_string:
         return True
     return False
